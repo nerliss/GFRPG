b5a4e3b7cef22247defb8951a8882adbd35722d5
Oleksandr Tkachov <nerliss228@gmail.com>
2025-10-07 00:52:37 +0300

    - Finalized MapImageGenerator

--- Source/InventoryProjectV3/Private/Map/RPGMapImageGenerator.cpp
+++ Source/InventoryProjectV3/Private/Map/RPGMapImageGenerator.cpp
@@ -37,8 +37,8 @@ ARPGMapImageGenerator::ARPGMapImageGenerator()
 	InstancedSM->SetupAttachment(Root);
 	
 	MapSize = 1000.f;
-	CameraLocation = FVector(0, 0, 0);
-	BorderScale = FVector(1, 1, 1);
+	CameraLocation = FVector(0.f, 0.f, 0.f);
+	BorderScale = FVector(3.f, 3.f, 3.f);
 }
 
 void ARPGMapImageGenerator::BeginPlay()
@@ -52,31 +52,57 @@ void ARPGMapImageGenerator::OnConstruction(const FTransform& Transform)
 {
 	Super::OnConstruction(Transform);
 
-	FVector TopLeftLocation = FVector((MapSize / 2.f * -1.f), MapSize / 2.f, 0.f);
-	FVector BottomLeftLocation = FVector(MapSize / 2.f, MapSize / 2.f, 0.f);
-	FVector TopRightLocation = FVector((MapSize / 2.f * -1.f), (MapSize / 2.f * -1.f), 0.f);
-	FVector BottomRightLocation = FVector(MapSize / 2.f, (MapSize / 2.f * -1.f), 0.f);
+	const FVector TopLeftLocation = FVector((MapSize / 2.f * -1.f), MapSize / 2.f, 0.f);
+	const FVector BottomLeftLocation = FVector(MapSize / 2.f, MapSize / 2.f, 0.f);
+	const FVector TopRightLocation = FVector((MapSize / 2.f * -1.f), (MapSize / 2.f * -1.f), 0.f);
+	const FVector BottomRightLocation = FVector(MapSize / 2.f, (MapSize / 2.f * -1.f), 0.f);
+	
 	TopLeftMarker->SetRelativeLocation(TopLeftLocation, false, nullptr, ETeleportType::ResetPhysics);
+	BottomLeftMarker->SetRelativeLocation(BottomLeftLocation, false, nullptr, ETeleportType::ResetPhysics);
+	TopRightMarker->SetRelativeLocation(TopRightLocation, false, nullptr, ETeleportType::ResetPhysics);
+	BottomRightMarker->SetRelativeLocation(BottomRightLocation, false, nullptr, ETeleportType::ResetPhysics);
+
+	const TArray<FVector> SplinePoints = {TopLeftLocation, TopRightLocation, BottomRightLocation, BottomLeftLocation};
+	Spline->SetSplinePoints(SplinePoints, ESplineCoordinateSpace::Local);
+
+	const FVector CameraLocationTempVector = (TopLeftMarker->GetComponentLocation() + BottomLeftMarker->GetComponentLocation() + TopRightMarker->GetComponentLocation() + BottomRightMarker->GetComponentLocation()) / 4.f; // Center of square
+	CameraLocation = FVector(CameraLocationTempVector.X, CameraLocationTempVector.Y, CameraLocationTempVector.Z + MapSize);
+
+	for (int i = 0; i < Spline->GetNumberOfSplinePoints(); i++)
+	{
+		Spline->SetSplinePointType(i, ESplinePointType::Linear);
+	}
+
+	const int32 InstancedMeshesNumber = FMath::TruncToInt(MapSize * 4.f / 100.f);
+	
+	InstancedSM->ClearInstances();
+	for (int i = 0; i < InstancedMeshesNumber; i++)
+	{
+		FVector LocationAtDistanceAlongSplinePoint = Spline->GetLocationAtDistanceAlongSpline((i * MapSize / 100.f), ESplineCoordinateSpace::Local);
+		FTransform InstanceTransform;
+		InstanceTransform.SetLocation(LocationAtDistanceAlongSplinePoint);
+		InstanceTransform.SetScale3D(BorderScale);
+		
+		InstancedSM->AddInstance(InstanceTransform);
+	}
 }
 
 void ARPGMapImageGenerator::TakeScreenshot()
 {
 #if WITH_EDITOR
 	UWidgetLayoutLibrary::RemoveAllWidgets(GetWorld());
-	// Delay 1
+	
 	const FRotator CameraRotation = FRotator(-90.f, -90.f, 0.f);
 	ACameraActor* SpawnedCamera = GetWorld()->SpawnActor<ACameraActor>(CameraLocation, CameraRotation);
-	// Delay 1
 	if (SpawnedCamera)
 	{
 		APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
 		if (PC)
 		{
 			PC->SetViewTarget(SpawnedCamera);
-			// Delay 1
 
 			UAutomationBlueprintFunctionLibrary::TakeHighResScreenshot(1920, 1080, "", SpawnedCamera);
-			// Delay 0.2
+			
 			SpawnedCamera->Destroy();
 		}
 	}

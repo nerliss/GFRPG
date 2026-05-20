1976ba6e0e886b17f20e64ee2d6822b49c0ac649
Oleksandr Tkachov <nerliss228@gmail.com>
2026-05-20 23:27:11 +0300

    - Added new ability archetype Displace

---
+++ Source/InventoryProjectV3/Public/Abilities/Archetypes/RPGAbilityDisplace.h
@@ -0,0 +1,17 @@
+// Oleksandr Tkachov 2021-2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Archetypes/RPGAbilityBase.h"
+#include "RPGAbilityDisplace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYPROJECTV3_API URPGAbilityDisplace : public URPGAbilityBase
+{
+	GENERATED_BODY()
+	
+};

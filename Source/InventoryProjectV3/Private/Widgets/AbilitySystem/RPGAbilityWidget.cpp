c9a1eb062ef4f22b3c66a3dfb902a150b378e4fa
Oleksandr Tkachov <nerliss228@gmail.com>
2026-05-25 23:06:54 +0300

    - Moved Ability Widget code to C++

--- Source/InventoryProjectV3/Private/Widgets/AbilitySystem/RPGAbilityWidget.cpp
+++ Source/InventoryProjectV3/Private/Widgets/AbilitySystem/RPGAbilityWidget.cpp
@@ -3,3 +3,137 @@
 
 #include "Widgets/AbilitySystem/RPGAbilityWidget.h"
 
+#include "Abilities/RPGAbilityComponent.h"
+#include "Abilities/RPGAbilityDefinitionData.h"
+#include "Components/Border.h"
+#include "Components/Image.h"
+#include "Components/Overlay.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+void URPGAbilityWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (AbilityDefinition)
+	{
+		if (AbilityThumbnailImage)
+		{
+			AbilityThumbnailImage->SetBrushFromTexture(AbilityDefinition->Icon);
+		}
+	}
+	else
+	{
+		if (AbilityThumbnailImage)
+		{
+			AbilityThumbnailImage->SetBrush(EmptyAbilitySlotBrush);
+		}
+	}
+}
+
+void URPGAbilityWidget::UpdateCooldownVisual()
+{
+	if (!CooldownText)
+	{
+		return;
+	}
+	
+	if (!CooldownProgressBar)
+	{
+		return;
+	}
+	
+	if (!Ability)
+	{
+		return;
+	}
+	
+	if (!AbilityComponent)
+	{
+		return;
+	}
+	
+	const float CooldownRemaining = AbilityComponent->GetCooldownRemainingForAbility(Ability);
+	const float CooldownPercent = AbilityComponent->GetCooldownPercentForAbility(Ability);
+	const int32 FractionalDigits = (CooldownRemaining <= 3.f) ? 1 : 0;
+	
+	FNumberFormattingOptions NumberFormattingOptions;
+	NumberFormattingOptions.MinimumFractionalDigits = 0;
+	NumberFormattingOptions.MaximumFractionalDigits = FractionalDigits;
+	
+	const FText Text = FText::AsNumber(CooldownRemaining, &NumberFormattingOptions);
+	CooldownText->SetText(Text);
+	
+	CooldownProgressBar->SetPercent(CooldownPercent);
+}
+
+void URPGAbilityWidget::StartCooldownVisual(URPGAbilityComponent* InAbilityComponent)
+{
+	if (!CooldownOverlay)
+	{
+		return;
+	}
+	
+	AbilityComponent = InAbilityComponent;
+	
+	CooldownOverlay->SetVisibility(ESlateVisibility::Visible);
+	
+	UpdateCooldownVisual();
+	
+	GetWorld()->GetTimerManager().SetTimer(CooldownTimerHandle, this, &URPGAbilityWidget::UpdateCooldownVisual, VisualsUpdateRate, true);
+}
+
+void URPGAbilityWidget::StopCooldownVisual()
+{
+	if (!CooldownFinished)
+	{
+		OnCooldownAnimationFinished();
+		return;
+	}
+	
+	FWidgetAnimationDynamicEvent CooldownFinishedAnimationDelegate;
+	CooldownFinishedAnimationDelegate.BindDynamic(this, &URPGAbilityWidget::OnCooldownAnimationFinished);
+	
+	UnbindAllFromAnimationFinished(CooldownFinished);
+	BindToAnimationFinished(CooldownFinished, CooldownFinishedAnimationDelegate);
+	
+	PlayAnimation(CooldownFinished, 0.f, 1, EUMGSequencePlayMode::Forward, 1.f);
+	
+	GetWorld()->GetTimerManager().ClearTimer(CooldownTimerHandle);
+}
+
+void URPGAbilityWidget::UpdateBackgroundBorder(const bool bAbilityInUse)
+{
+	if (!BackgroundBorder)
+	{
+		return;
+	}
+	
+	BackgroundBorder->SetBrushColor(bAbilityInUse ? BackgroundBorderColorInUse : BackgroundBorderColorDefault);
+}
+
+void URPGAbilityWidget::OnCooldownAnimationFinished()
+{
+	if (!CooldownOverlay)
+	{
+		return;
+	}
+	
+	if (!AbilityComponent)
+	{
+		return;
+	}
+	
+	if (!Ability)
+	{
+		return;
+	}
+	
+	if (AbilityComponent->IsAbilityOnCooldown(Ability))
+	{
+		return;
+	}
+	
+	CooldownOverlay->SetRenderScale(FVector2D(1.f));
+	CooldownOverlay->SetVisibility(ESlateVisibility::Collapsed);
+}
--- Source/InventoryProjectV3/Public/Widgets/AbilitySystem/RPGAbilityWidget.h
+++ Source/InventoryProjectV3/Public/Widgets/AbilitySystem/RPGAbilityWidget.h
@@ -6,6 +6,15 @@
 #include "Blueprint/UserWidget.h"
 #include "RPGAbilityWidget.generated.h"
 
+class UProgressBar;
+class URPGAbilityBase;
+class UImage;
+class UOverlay;
+class UTextBlock;
+class UBorder;
+class URPGAbilityDefinitionData;
+class URPGAbilityComponent;
+
 /**
  * 
  */
@@ -14,4 +23,68 @@ class INVENTORYPROJECTV3_API URPGAbilityWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	
+	virtual void NativeConstruct() override;
+	
+	void UpdateCooldownVisual();
+	void StartCooldownVisual(URPGAbilityComponent* InAbilityComponent);
+	void StopCooldownVisual();
+	void UpdateBackgroundBorder(const bool bAbilityInUse);
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UImage* AbilityThumbnailImage;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UBorder* BackgroundBorder;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UOverlay* CooldownOverlay;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UTextBlock* CooldownText;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UProgressBar* CooldownProgressBar;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UOverlay* ToggleOverlay;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UTextBlock* HotkeyText;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UWidgetAnimation* CooldownFinished;
+	
+	UPROPERTY(BlueprintReadOnly, meta=(BindWidget))
+	UWidgetAnimation* AbilityUsed;
+	
+	UPROPERTY(BlueprintReadOnly)
+	URPGAbilityDefinitionData* AbilityDefinition;
+	
+	UPROPERTY(BlueprintReadOnly)
+	URPGAbilityComponent* AbilityComponent;
+
+	UPROPERTY(BlueprintReadOnly)
+	URPGAbilityBase* Ability;
+	
+	UPROPERTY(BlueprintReadOnly)
+	FTimerHandle CooldownTimerHandle;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ability")
+	FSlateBrush EmptyAbilitySlotBrush;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ability")
+	FLinearColor BackgroundBorderColorInUse = FLinearColor::Yellow;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ability")
+	FLinearColor BackgroundBorderColorDefault = FLinearColor::Gray;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Ability")
+	float VisualsUpdateRate = 0.1f;
+	
+private:
+	
+	UFUNCTION()
+	void OnCooldownAnimationFinished();
 };
